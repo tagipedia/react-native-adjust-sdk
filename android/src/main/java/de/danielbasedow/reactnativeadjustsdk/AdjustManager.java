053742a621a4fdce3244716ff382690068d253f1
danielbasedow <daniel.basedow@gmail.com>
2016-02-01 10:30:27 +0100

    add event tracking for android

--- android/src/main/java/de/danielbasedow/reactnativeadjustsdk/AdjustManager.java
+++ android/src/main/java/de/danielbasedow/reactnativeadjustsdk/AdjustManager.java
@@ -2,6 +2,7 @@ package de.danielbasedow.reactnativeadjustsdk;
 
 import com.adjust.sdk.Adjust;
 import com.adjust.sdk.AdjustConfig;
+import com.adjust.sdk.AdjustEvent;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -23,5 +24,11 @@ public class AdjustManager extends ReactContextBaseJavaModule {
         String environment = config.getString("environment");
         AdjustConfig cfg = new AdjustConfig(this.getReactApplicationContext(), token, environment);
         Adjust.onCreate(cfg);
+    }
+
+    @ReactMethod
+    public void trackEvent(ReadableMap event) {
+        AdjustEvent e = new AdjustEvent(event.getString("token"));
+        Adjust.trackEvent(e);
     }
 }
